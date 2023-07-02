148e3e4dd59117be696b0bc8f83fef40ca767611
Alvaro <alvaromagu2002@gmail.com>
2023-07-02 20:45:15 +0200

    Fix render two random quotes

--- src/hooks/use-random-quote.ts
+++ src/hooks/use-random-quote.ts
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react'
 import { type Quote } from '../types/quote'
 import { getRandomQuote } from '../services/random-quote'
+import { AbortError } from '../types/exceptions/abort-error'
 
 export function useRandomQuote () {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [quote, setQuote] = useState<Quote | null>()
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     setIsLoading(true)
-    getRandomQuote()
+    const abortController = new AbortController()
+
+    getRandomQuote(abortController)
       .then(randomQuote => {
         setQuote(randomQuote)
+        setIsLoading(false)
       })
       .catch(err => {
+        if (err instanceof AbortError) return
         setError(err.message)
+        setIsLoading(false)
       })
-      .finally(() => { setIsLoading(false) })
-  }, [setQuote, setIsLoading, setError])
+
+    return () => {
+      abortController.abort()
+    }
+  }, [])
 
   return { isLoading, quote, error }
 }
--- src/services/random-quote.ts
+++ src/services/random-quote.ts
@@ -1,9 +1,10 @@
+import { AbortError } from '../types/exceptions/abort-error'
 import { NotFound } from '../types/exceptions/not-found'
 import { UnexpectedError } from '../types/exceptions/unexpected-error'
 import { type Quote } from '../types/quote'
 
-export async function getRandomQuote (): Promise<Quote> {
-  return await fetch('https://quote-garden.onrender.com/api/v3/quotes/random')
+export async function getRandomQuote (abortController?: AbortController): Promise<Quote> {
+  return await fetch('https://quote-garden.onrender.com/api/v3/quotes/random', { signal: abortController?.signal })
     .then(async res => await res.json())
     .then(res => {
       const { data } = res
@@ -11,6 +12,7 @@ export async function getRandomQuote (): Promise<Quote> {
       return data[0] as Quote
     })
     .catch(err => {
+      if (err.name === 'AbortError') throw new AbortError()
       throw new UnexpectedError(err.message)
     })
 }
